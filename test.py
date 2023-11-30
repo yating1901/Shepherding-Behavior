91aa6a1fd08d5980569f50ca9443a6090806dff8
yating1901 <zhengyating@mail.bnu.edu.cn>
2023-12-01 00:25:31 +0100

    remove the data recording the self-orgainzed flocking inside TICK time

--- test.py
+++ test.py
@@ -12,10 +12,10 @@ from basic.interaction import evolve, make_preodic_boundary
 from basic.save_data import save_data
 from basic.draw_states import draw_state, draw_state_single
 from basic.draw import draw_single, draw_dynamic, plot_snapshot
-from basic.create_network import create_metric_network
+from basic.create_network import create_metric_network, create_topological_network
 
-N_sheep = 100
-N_shepherd = 2
+N_sheep = 200
+N_shepherd = 1
 Space_x = 150
 Space_y = 150
 
@@ -26,44 +26,60 @@ Target_size = 100  # radius
 Boundary_x = Target_place_x + Target_size
 Boundary_y = Target_place_y + Target_size
 
-Iterations = 50000
+
 TICK = 10000
-Repetition = 3
+Iterations = 13
+
+
+Repetition = 0
+
+Num_nearst_neighbor = 5
 
 if __name__ == '__main__':
     agents = initiate(N_sheep, Space_x, Space_y, Target_size)
     shepherd = initiate_shepherd(0, N_sheep)
-    Data_agents = np.zeros((agents.shape[0], agents.shape[1], Iterations), float)
-    Data_shepherds = np.zeros((shepherd.shape[0], shepherd.shape[1], Iterations), float)
-    # Map_agents = np.zeros((N_sheep, N_sheep, Iterations), float)
-    Max_agents_indexes = np.zeros((shepherd.shape[0], Iterations), int)
-    Final_iterations = Iterations
-
-    for tick in range(Iterations):
-        if tick == TICK:
-            shepherd = initiate_shepherd(N_shepherd, N_sheep)  # add shepherd at certain tick
-            Data_shepherds = np.zeros((shepherd.shape[0], shepherd.shape[1], Iterations), float)
-            Max_agents_indexes = np.zeros((N_shepherd, Iterations), int)
-
-        agents_update, shepherd_update, max_agents_indexes = evolve(agents, shepherd, Target_place_x, Target_place_y, Target_size)
-
+    # self-organized flocking
+    for tick in range(TICK):
+        agents_update, shepherd_update, max_agents_indexes = evolve(agents, shepherd, Target_place_x,
+                                                                    Target_place_y, Target_size)
         agents = agents_update
         shepherd = shepherd_update
+    # prepare the shepherd and record data
+    shepherd = initiate_shepherd(N_shepherd, N_sheep)
+    Data_agents = np.zeros((agents.shape[0], agents.shape[1], Iterations), float)
+    Data_shepherds = np.zeros((shepherd.shape[0], shepherd.shape[1], Iterations), float)
+    Max_agents_indexes = np.zeros((N_shepherd, Iterations), int)
+    Final_tick = Iterations
+    # continue the sheep data with shepherd
+    for tick in range(Iterations):
+        # start evolve function
+        agents_update, shepherd_update, max_agents_indexes = evolve(agents, shepherd, Target_place_x, Target_place_y,
+                                                                    Target_size)
+        # update data
+        agents = agents_update
+        shepherd = shepherd_update
+        # save data
         Data_agents[:, :, tick] = agents
         Data_shepherds[:, :, tick] = shepherd
-        # Map_agents[:, :, tick] = map
-        Max_agents_indexes[:, tick] = max_agents_indexes    #only two dimension
-
+        Max_agents_indexes[:, tick] = max_agents_indexes  # only two dimension
+        # stop program if all the sheep are in the "staying" mode;
         if sum(agents[:, 21]) == N_sheep:   # finish
-            Final_iterations = tick
+            Final_tick = tick
             break
-    draw_dynamic(Final_iterations, Data_agents, Data_shepherds, Boundary_x, Boundary_y, Target_place_x, Target_place_y, Target_size)
-    print("Repetition=", Repetition, "N_Shepherd=", N_shepherd, "N_sheep=", N_sheep, "Final_tick=", Final_iterations)
-    print("L0=", shepherd[0][3], "L1=", shepherd[0][5], "L2=", shepherd[0][12], "L3=", shepherd[0][19])
+        # try to create network
+        # topological_network = create_topological_network(agents, Num_nearst_neighbor)
+        # metric_network = create_metric_network(agents, agents[0][5], np.pi)   # Fov not used
+        # print("topological_network:", topological_network)
+        # print("metric_network:", metric_network)
+
+    # draw_dynamic(Final_tick, Data_agents, Data_shepherds, Boundary_x, Boundary_y, Target_place_x, Target_place_y, Target_size)
+    # print("Repetition=", Repetition, "N_Shepherd=", N_shepherd, "N_sheep=", N_sheep, "Final_tick=", Final_tick)
+
+    # print("L0=", shepherd[0][3], "L1=", shepherd[0][5], "L2=", shepherd[0][12], "L3=", shepherd[0][19])
     # plot_snapshot(agents, shepherd, Repetition, Boundary_x, Boundary_y, Target_place_x, Target_place_y, Target_size)
 
-    # draw_state(Final_iterations, Data_shepherds)
+    # draw_state(Final_tick, Data_shepherds)
 
-    # draw_state_single(Final_iterations, Data_shepherds)
+    # draw_state_single(Final_tick, Data_shepherds)
 
-    # save_data(N_sheep, N_shepherd, Repetition, Final_iterations, Data_agents, Data_shepherds)
+    save_data(N_sheep, N_shepherd, Repetition, Final_tick, Data_agents, Data_shepherds)
