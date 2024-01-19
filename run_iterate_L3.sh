cb0e11480ed8d61a48c5f024be26abe5ce032713
yating1901 <zhengyating@mail.bnu.edu.cn>
2024-01-19 17:16:15 +0100

    add repetitions in the main file

--- run_iterate_L3.sh
+++ run_iterate_L3.sh
@@ -1,14 +1,14 @@
 count=0
 TICK=10000
-for N_sheep in $(seq 100 100 500)
+for N_sheep in $(seq 400 100 500)
 do
   for N_shepherd in $(seq 1 1 5)
   do
-    for Repetition in $(seq 1 1 10)
+    for Repetition in 5
     do
       for Iterations in 160000
       do
-        for L3 in $(seq 0 5 25)
+        for L3 in 0
         do
           nohup python "main_interface.py" $N_sheep $N_shepherd $Repetition $Iterations $TICK $L3 >/dev/null 2>&1 &
           echo "N_sheep: $N_sheep";
